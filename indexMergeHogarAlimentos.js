97f126415b3922a2b091b0d98036429fac3dc1f6
zambranoluis <130914970+zambranoluis@users.noreply.github.com>
2023-07-19 02:23:57 -0400

    Update indexMergeHogarAlimentos.js

--- indexMergeHogarAlimentos.js
+++ indexMergeHogarAlimentos.js
@@ -387,14 +387,6 @@ function principal (){
 
 principal();
 
-// function cambiarCategoriaDerecha(){
-//     indiceCategoria++;
-//     console.log(indiceCategoria);
-
-// }
-
-// let boton = document.getElementById("botonCambiarCategoriaDerecha");
-// boton.addEventListener('click', cambiarCategoriaDerecha);
 
 
 
@@ -407,58 +399,34 @@ principal();
 
 
 
+// Reloj y Calendario - inicio
 
+function showClock(){
+    let date = new Date();
+    let hours = formatTime(date.getHours());
+    let minutes = formatTime(date.getMinutes());
+    let seconds = formatTime(date.getSeconds());
+    let clock = `${hours} : ${minutes} : ${seconds}`;
+    document.getElementById("clock").textContent = clock;
+}
 
+function showCalendar(){
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let calendar = `${day}/${month}/${year}`;
+    document.getElementById("calendar").textContent = calendar;
+}
 
+function formatTime(time){
+    if (time < 10) {
+        time = "0" + time;
+    }
+    return time;
+}
 
+setInterval(showClock, 1000);
+setInterval(showCalendar, 1000);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Reloj y Calendario - inicio
-
-// function showClock(){
-//     let date = new Date();
-//     let hours = formatTime(date.getHours());
-//     let minutes = formatTime(date.getMinutes());
-//     let seconds = formatTime(date.getSeconds());
-//     let clock = `${hours} : ${minutes} : ${seconds}`;
-//     document.getElementById("clock").textContent = clock;
-// }
-
-// function showCalendar(){
-//     let date = new Date();
-//     let day = date.getDate();
-//     let month = date.getMonth() + 1;
-//     let year = date.getFullYear();
-//     let calendar = `${day}/${month}/${year}`;
-//     document.getElementById("calendar").textContent = calendar;
-// }
-
-// function formatTime(time){
-//     if (time < 10) {
-//         time = "0" + time;
-//     }
-//     return time;
-// }
-
-// setInterval(showClock, 1000);
-// setInterval(showCalendar, 1000);
-
-// // Reloj y Calendario - fin
+// Reloj y Calendario - fin
